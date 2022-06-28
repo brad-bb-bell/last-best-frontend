360401c13377b1a911aa9b16ed550782971c6c16
Brad Bell <brad.bb.bell@gmail.com>
2022-06-28 09:43:20 -0600

    Adds user authentication and signup/login/logout views

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ResortView from "../views/ResortView.vue";
 import EventView from "../views/EventView.vue";
+import SignupView from "../views/SignupView.vue";
+import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
     path: "/resorts/:id",
@@ -26,6 +21,21 @@ const routes = [
     path: "/events/:id",
     name: "events-show",
     component: EventView,
+  },
+  {
+    path: "/signup/",
+    name: "signup",
+    component: SignupView,
+  },
+  {
+    path: "/login/",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/logout/",
+    name: "logout",
+    component: LogoutView,
   },
 ];
 
