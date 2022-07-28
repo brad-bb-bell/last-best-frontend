748713878ed6f6ba4a93379e3cc106a9db006ea2
Brad Bell <brad.bb.bell@gmail.com>
2022-07-28 14:38:15 -0600

    changes backend in main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://blooming-retreat-99979.herokuapp.com/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://calm-sea-15156.herokuapp.com/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
